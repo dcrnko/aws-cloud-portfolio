2edbce2e12759ab42bb213dd219da85d02716021
dcrnko <crnkodav@gmail.com>
2024-09-30 19:46:40 +0200

    changing JS

--- js/js.js
+++ js/js.js
@@ -1,13 +1,31 @@
 document.getElementById('toggleLink').addEventListener('click', function(e) {
-    e.preventDefault();
-    const section = document.getElementById('toggleSection');
-    
-    if (section.style.display === 'none' || section.style.display === '') {
-      section.style.display = 'block';
-    } else {
-      section.style.display = 'none';
-    }
-  });
-  function scrollToNextSection() {
-    document.getElementById('next-section').scrollIntoView({ behavior: 'smooth' });
+  e.preventDefault();
+  const section = document.getElementById('toggleSection');
+  
+  if (section.style.display === 'none' || section.style.display === '') {
+    section.style.display = 'block';
+  } else {
+    section.style.display = 'none';
   }
+});
+function scrollToNextSection() {
+  document.getElementById('next-section').scrollIntoView({ behavior: 'smooth' });
+}
+
+function toggleContent(post) {
+  const content = post.querySelector('.blog-content');
+  if (content.style.display === "none") {
+    content.style.display = "block"; // Show content
+  } else {
+    content.style.display = "none"; // Hide content
+  }
+}
+
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("https://c6wbwoqerqnmuuoxkeluqvmhni0jnwsk.lambda-url.eu-central-1.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = ` This page has ${data} Views!`;
+}
+
+updateCounter();
