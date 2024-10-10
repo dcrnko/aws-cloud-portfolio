6e4fb8e63964187949dc08d6ae72e48601623709
dcrnko <crnkodav@gmail.com>
2024-10-10 15:32:47 +0200

    nexttest

--- js/js.js
+++ js/js.js
@@ -7,19 +7,6 @@ async function updateCounter() {
 
 updateCounter();
 
-document.getElementById('toggleLink').addEventListener('click', function(e) {
-  e.preventDefault();
-  const section = document.getElementById('toggleSection');
-  
-  if (section.style.display === 'none' || section.style.display === '') {
-    section.style.display = 'block';
-  } else {
-    section.style.display = 'none';
-  }
-});
-function scrollToNextSection() {
-  document.getElementById('next-section').scrollIntoView({ behavior: 'smooth' });
-}
 
 function toggleContent(post) {
   const content = post.querySelector('.blog-content');
