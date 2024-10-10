16a34e609ce7afb85bd256a1f10396bb77004c1c
dcrnko <crnkodav@gmail.com>
2024-10-10 13:47:26 +0200

    counter2_final

--- js/js.js
+++ js/js.js
@@ -23,7 +23,7 @@ function toggleContent(post) {
 
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://c6wbwoqerqnmuuoxkeluqvmhni0jnwsk.lambda-url.eu-central-1.on.aws/");
+    let response = await fetch("https://qsl325knfxo2bsg5d5mbueokqu0cqmqk.lambda-url.eu-central-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` the portfolio has ${data} views`;
 }
