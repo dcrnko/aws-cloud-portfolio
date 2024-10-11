10718e63d0407dacb74c46c67157206f79d3bd37
dcrnko <crnkodav@gmail.com>
2024-10-11 10:25:13 +0200

    narrowplusother

--- js/js.js
+++ js/js.js
@@ -2,10 +2,18 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://qsl325knfxo2bsg5d5mbueokqu0cqmqk.lambda-url.eu-central-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = ` the portfolio has ${data} views`;
+    counter.innerHTML = ` my portfolio has ${data} views`;
 }
 
 updateCounter();
+
+// Function to scroll down to the next section
+document.getElementById('scrollButton').addEventListener('click', function() {
+  // Get the second section's position
+  const section2 = document.getElementById('blog');
+  section2.scrollIntoView({ behavior: 'smooth' });
+});
+
 
 
 function toggleContent(post) {
