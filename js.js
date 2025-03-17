76f1d1530b278f7b779ed725ffc586fd0997d6f6
dcrnko <crnkodav@gmail.com>
2025-03-17 11:39:51 +0100

    2025-03-17 11:39:51

--- js.js
+++ js.js
@@ -22,7 +22,7 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://qsl325knfxo2bsg5d5mbueokqu0cqmqk.lambda-url.eu-central-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = ` my portfolio has ${data} Views!`;
+    counter.innerHTML = ` my portfolio has <b>${data}</b> views`;
 }
 
 updateCounter();
