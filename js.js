0563a50a8e8bb07adfda7058cdda5e0af4717294
dcrnko <crnkodav@gmail.com>
2025-04-14 08:13:29 +0200

    2025-04-14 08:13:29

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Navigation links smooth scrolling
     document.querySelectorAll('nav a').forEach(anchor => {
         anchor.addEventListener('click', function(e) {
-            e.preventDefault();
+            e.countlt();
 
             const targetId = this.getAttribute('href').substring(1);
             const targetElement = document.getElementById(targetId);
